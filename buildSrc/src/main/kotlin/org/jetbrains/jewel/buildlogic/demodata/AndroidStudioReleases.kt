288bc722cd05569cc2e1f10e2830bbd7a399c1f2
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-12-07 17:18:36 +0100

    Reproduce KT-73761

--- buildSrc/src/main/kotlin/org/jetbrains/jewel/buildlogic/demodata/AndroidStudioReleases.kt
+++ buildSrc/src/main/kotlin/org/jetbrains/jewel/buildlogic/demodata/AndroidStudioReleases.kt
@@ -1,8 +1,8 @@
 package org.jetbrains.jewel.buildlogic.demodata
 
 import com.squareup.kotlinpoet.ClassName
-import gradle.kotlin.dsl.accessors._327d2b3378ed6d2c1bec5d20438f90c7.kotlin
-import gradle.kotlin.dsl.accessors._327d2b3378ed6d2c1bec5d20438f90c7.sourceSets
+//import gradle.kotlin.dsl.accessors._327d2b3378ed6d2c1bec5d20438f90c7.kotlin
+//import gradle.kotlin.dsl.accessors._327d2b3378ed6d2c1bec5d20438f90c7.sourceSets
 import java.io.File
 import java.net.URI
 import kotlinx.serialization.json.Json
@@ -27,9 +27,9 @@ open class StudioVersionsGenerationExtension(project: Project) {
     val targetDir: DirectoryProperty =
         project.objects
             .directoryProperty()
-            .convention(
+            /*.convention(
                 project.layout.dir(project.provider { project.sourceSets.named("main").get().kotlin.srcDirs.first() })
-            )
+            )*/
 
     val resourcesDirs: SetProperty<File> =
         project.objects
